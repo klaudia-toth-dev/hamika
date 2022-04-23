f95821c2c1c2e5481f6147486adc03efe35ec883
Klaudia Tóth <klaudia.toth.dev@gmail.com>
2022-04-23 03:22:19 +0200

    Changes from mac

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,37 +2,47 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/userModel");
 
-router.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
-  const newUser = new User({ name, email, password });
-  console.log(newUser);
-  try {
-    newUser.save();
-    res.send("User registered successfully");
-  } catch (error) {
-    return res.status(400).json({ message: error });
-  }
+router.get("/getallusers", async(req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({ message: error });
+    }
 });
 
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await User.find({ email, password });
-
-    if (user.length > 0) {
-      const currentUser = {
-        name: user[0].name,
-        email: user[0].email,
-        isAdmin: user[0].isAdmin,
-        _id: user[0]._id,
-      };
-      res.send(currentUser);
-    } else {
-      return res.status(400).json({ message: "User login failed" });
+router.post("/register", async(req, res) => {
+    const { name, email, password } = req.body;
+    const newUser = new User({ name, email, password });
+    console.log(newUser);
+    try {
+        newUser.save();
+        res.send("User registered successfully");
+    } catch (error) {
+        return res.status(400).json({ message: error });
     }
-  } catch (error) {
-    return res.status(400).json({ message: "Something went wrong" });
-  }
+});
+
+router.post("/login", async(req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.find({ email, password });
+
+        if (user.length > 0) {
+            const currentUser = {
+                name: user[0].name,
+                email: user[0].email,
+                isAdmin: user[0].isAdmin,
+                _id: user[0]._id,
+            };
+            res.send(currentUser);
+        } else {
+            return res.status(400).json({ message: "User login failed" });
+        }
+    } catch (error) {
+        return res.status(400).json({ message: "Something went wrong" });
+    }
 });
 
 module.exports = router;
