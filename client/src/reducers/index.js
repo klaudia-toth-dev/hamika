93f7e7be13a0232c6722e9d47cd429f3fbdd296b
clautoth <toth.klaudia7@gmail.com>
2022-05-06 20:51:38 +0200

    Store and reducers index changes

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,8 +3,37 @@ import alert from "./alert";
 import auth from "./auth";
 import profile from "./profile";
 
+import {
+  getAllItemsReducer,
+  addItemReducer,
+  editItemReducer,
+  getItemByIdReducer,
+} from "./itemReducers";
+import { cartReducer } from "./cartReducer";
+import {
+  getAllUsersReducer,
+  registerUserReducer,
+  loginUserReducer,
+} from "./userReducers";
+import {
+  placeOrderReducer,
+  getUserOrdersReducer,
+  getAllOrdersReducer,
+} from "./orderReducers";
+
 export default combineReducers({
   alert,
   auth,
   profile,
+  // getAllItemsReducer: getAllItemsReducer,
+  // cartReducer: cartReducer,
+  // registerUserReducer: registerUserReducer,
+  // getAllUsersReducer: getAllUsersReducer,
+  // loginUserReducer: loginUserReducer,
+  // addItemReducer: addItemReducer,
+  // getItemByIdReducer: getItemByIdReducer,
+  // placeOrderReducer: placeOrderReducer,
+  // getUserOrdersReducer: getUserOrdersReducer,
+  // editItemReducer: editItemReducer,
+  // getAllOrdersReducer: getAllOrdersReducer,
 });
--- client/src/store.js
+++ client/src/store.js
@@ -1,49 +1,13 @@
 // import { combineReducers } from "redux";
 
-// import { createStore, applyMiddleware } from "redux";
-// import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
-// import { composeWithDevTools } from "redux-devtools-extension";
-// import {
-//   getAllItemsReducer,
-//   addItemReducer,
-//   editItemReducer,
-//   getItemByIdReducer,
-// } from "./reducers/itemReducers";
-// import { cartReducer } from "./reducers/cartReducer";
-// import {
-//   getAllUsersReducer,
-//   registerUserReducer,
-//   loginUserReducer,
-// } from "./reducers/userReducers";
-// import {
-//   placeOrderReducer,
-//   getUserOrdersReducer,
-//   getAllOrdersReducer,
-// } from "./reducers/orderReducers";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-// const finalReducer = combineReducers({
-//   getAllItemsReducer: getAllItemsReducer,
-//   cartReducer: cartReducer,
-//   registerUserReducer: registerUserReducer,
-//   getAllUsersReducer: getAllUsersReducer,
-//   loginUserReducer: loginUserReducer,
-//   addItemReducer: addItemReducer,
-//   getItemByIdReducer: getItemByIdReducer,
-//   placeOrderReducer: placeOrderReducer,
-//   getUserOrdersReducer: getUserOrdersReducer,
-//   editItemReducer: editItemReducer,
-//   getAllOrdersReducer: getAllOrdersReducer,
-// });
+import rootReducer from "./reducers";
 
-// const cartItems = localStorage.getItem("cartItems")
-//   ? JSON.parse(localStorage.getItem("cartItems"))
-//   : [];
-
-// const currentUser = localStorage.getItem("currentUser")
-//   ? JSON.parse(localStorage.getItem("currentUser"))
-//   : null;
-
+const initialState = {};
 // const initialState = {
 //   cartReducer: {
 //     cartItems: cartItems,
@@ -53,23 +17,13 @@
 //   },
 // };
 
-// console.log(currentUser, "STORE USER");
+// const cartItems = localStorage.getItem("cartItems")
+//   ? JSON.parse(localStorage.getItem("cartItems"))
+//   : [];
 
-// const composeEnchanters = composeWithDevTools({});
-// const store = createStore(
-//   finalReducer,
-//   initialState,
-//   composeEnchanters(applyMiddleware(thunk))
-// );
-
-// export default store;
-
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers";
-
-const initialState = {};
+// const currentUser = localStorage.getItem("currentUser")
+//   ? JSON.parse(localStorage.getItem("currentUser"))
+//   : null;
 
 const middleware = [thunk];
 
