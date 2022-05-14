690015fe617b448f9f2803f200d0506a335ec0b7
clautoth <toth.klaudia7@gmail.com>
2022-05-14 20:50:07 +0200

    Chane sec policy 2

--- server.js
+++ server.js
@@ -21,7 +21,23 @@ app.use(express.json({ extended: false }));
 // );
 app.use(
   helmet({
-    contentSecurityPolicy: false,
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'"],
+        styleSrc: ["'self'", "https://fonts.googleapis.com", "'unsafe-inline'"],
+        imgSrc: ["'self'", "data:"],
+        connectSrc: [
+          "'self'",
+          "https://ourDomain.us.auth0.com/oauth/token",
+          "https://ourDomain.azure-api.net/fields/request/paths/invoke",
+        ],
+        fontSrc: ["'self'", "https://fonts.gstatic.com"],
+        objectSrc: ["'self'"],
+        mediaSrc: ["'self'"],
+        frameSrc: ["'self'", "ourDomain.us.auth0.com"],
+      },
+    },
   })
 );
 
