624a6b77f3d101721922d5526e05f6e0a11ad7cc
clautoth <toth.klaudia7@gmail.com>
2022-05-16 13:09:15 +0200

    Welcome content

--- client/src/screens/WelcomeScreen.js
+++ client/src/screens/WelcomeScreen.js
@@ -44,9 +44,10 @@ export default function WelcomeScreen() {
           </div>
           <div className="homeDiv">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-              vel semper erat, vitae iaculis odio. In nisi tortor, iaculis sed
-              porta elementum, porttitor eget eros.
+              Welcome to Hamika, the restaurant that will always succeed in
+              aweing you. Come and visit one of Hungary's top restaurants in
+              person, or order online and enjoy luxury being delivered to your
+              door.
             </p>
             <a className="btn" href="/menu">
               ORDER ONLINE
@@ -74,38 +75,37 @@ export default function WelcomeScreen() {
           <h1>WHO WE ARE</h1>
           <p>Pictures on the left side of the screen.</p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel
-            semper erat, vitae iaculis odio. In nisi tortor, iaculis sed porta
-            elementum, porttitor eget eros. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit. Integer vel semper erat, vitae iaculis
-            odio. In nisi tortor, iaculis sed porta elementum, porttitor eget
-            eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Integer vel semper erat, vitae iaculis odio. In nisi tortor, iaculis
-            sed porta elementum, porttitor eget eros. Lorem ipsum dolor sit
-            amet, consectetur adipiscing elit. Integer vel semper erat, vitae
-            iaculis odio. In nisi tortor, iaculis sed porta elementum, porttitor
-            eget eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Integer vel semper erat, vitae iaculis odio. In nisi tortor, iaculis
-            sed porta elementum, porttitor eget eros.
+            At Hamika Restaurant our desire is to contribute to the community
+            that has so loyally supported us. Our vision is to create a dining
+            experience that foremost, feels like a place that you decide to call
+            yours, and that you choose to share with your family and friends. We
+            feel that we’re living within that vision because in the Summer of
+            2016, we were awarded the Andreas Matthew Foundation Award, claiming
+            the "Family Friendly Restaurant" title. Thank you! Our promise is to
+            continue to be that comfortable environment for you to come and
+            break bread with your friends and family. We’re committed to
+            excellence in food preparation, flavour and presentation by using
+            quality, fresh ingredients. We’re passionate about creating premium
+            homemade meals that satiate the appetite, satisfy the taste buds,
+            and surprise even the most prepared.
           </p>
         </section>
         <section className="restaurant-section welcome-section" id="restaurant">
           <h1>ABOUT THE RESTAURANT</h1>
           <p>Pictures on the top.</p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel
-            semper erat, vitae iaculis odio. In nisi tortor, iaculis sed porta
-            elementum, porttitor eget eros. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit. Integer vel semper erat, vitae iaculis
-            odio. In nisi tortor, iaculis sed porta elementum, porttitor eget
-            eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Integer vel semper erat, vitae iaculis odio. In nisi tortor, iaculis
-            sed porta elementum, porttitor eget eros. Lorem ipsum dolor sit
-            amet, consectetur adipiscing elit. Integer vel semper erat, vitae
-            iaculis odio. In nisi tortor, iaculis sed porta elementum, porttitor
-            eget eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Integer vel semper erat, vitae iaculis odio. In nisi tortor, iaculis
-            sed porta elementum, porttitor eget eros.
+            Hamika was opened in 1996 by a young couple. The original location
+            of the restaurant was meant to be Budapest, but as the founders fell
+            in love with the incredible sights of Lenti, they decided to open
+            the restaurant in this picturesque town. Hamika's one of a kind
+            building was originally a family house, giving the perfect
+            foundation to becoming a restaurant of the edge of this seemingly
+            remote but life-filled location. Hamika offers the most mind
+            boggling meals you have ever tasted. You can try your favorites with
+            a double twist and enjoy the perfect beverage on the side.
+            Regardless of what you are drinking we will custom make you
+            cocktails that will emphasize your choice of food in ways you have
+            not seen before.
           </p>
         </section>
         <section className="contact-section welcome-section" id="contact">
